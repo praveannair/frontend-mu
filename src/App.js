416732eac4bcdb8094067570274749b642d54983
praveen <nairxprv@gmail.com>
2024-11-22 19:32:29 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const url = "http://localhost:8080/";
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await axios.get(url);
+    setProducts(response.data);
+  };
+
+  const addProduct = async () => {
+    await axios.post(url,{name,price});
+    getProducts()
+  };
+
+  const deleteProduct = async (id) => {
+    await axios.delete(url + id);
+    console.log(url+id)
+    getProducts()
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Application</h1>
+      <hr></hr>
+      <h3>Product Page</h3>
+      <input
+        type="text"
+        placeholder="Enter Product"
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <input
+        type="number"
+        placeholder="Enter Price"
+        onChange={(e) => setPrice(e.target.value)}
+      ></input>
+      <button onClick={addProduct}>Add</button>
+      <ul>
+        {products &&
+          products.map((value, index) => (
+            <li key={index}>
+              <b>{value.name}</b>-{value.price} - <button onClick={()=>deleteProduct(value._id)}>Delete</button>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
